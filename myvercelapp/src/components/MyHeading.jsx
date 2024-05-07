2f45e86aa5a9b5166d078131a32322b7ada1441f
Kushagra <er.kush19@gmail.com>
2024-05-07 11:07:03 +0530

    new changes to heading

--- myvercelapp/src/components/MyHeading.jsx
+++ myvercelapp/src/components/MyHeading.jsx
@@ -1,4 +1,4 @@
-const MyHeading = () => <h1>Heading</h1>;
+const MyHeading = () => <h1>My Heading</h1>;
 //Tip#2a: short hand for small components 
 //Tip#2b: shortcut - rafce
 //Tip#2c: cant add JS without return
